bce98691e6970a758b1ad22a5207d09c0feaf33e
Abdullah Khdir <Abdullah.khdir@telekom.de>
2024-10-10 12:15:20 +0200

    after build

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// metro.config.js
+module.exports = {
+    resolver: {
+        // This configuration is needed to specify the entry file of your app
+        sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+    },
+  };
+  
